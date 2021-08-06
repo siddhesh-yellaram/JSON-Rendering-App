c14e933ea1a3563862ec0bea8c4149093731927c
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 02:23:20 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsonRenderingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Json Obj
+        public ActionResult JsonPage()
+        {
+            return Json(new { Name =  Request.QueryString["developer"], Company = "Xyz"},JsonRequestBehavior.AllowGet);
+        }
+    }
+}
